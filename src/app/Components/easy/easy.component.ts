c633b195996471d8bdef3fbb762f64d4bef295d5
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2022-02-16 02:44:11 +0530

    added audio files

--- src/app/Components/easy/easy.component.ts
+++ src/app/Components/easy/easy.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-easy',
   templateUrl: './easy.component.html',
@@ -7,21 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EasyComponent implements OnInit {
 
-  isButtonVisible = true;
-  errorr = 0;
-  flipss = 0;
-
+  
   constructor() {
-
+    
   }
 
   ngOnInit(): void {
+      let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+      let cards = Array.from(document.getElementsByClassName('card'));
+      
 
+      overlays.forEach(overlay =>{
+        overlay.addEventListener('click',()=>{
+          overlay.classList.remove('visible');
+          // game.startGame();
+        });
+      });
+      
+      cards.forEach(card => {
+        card.addEventListener('click', () => {
+          // game.flipCard(card);
+        });
+    });
   }
+
   
-  onStart() {
-    this.isButtonVisible = false;
-   
+  
+  onStart() {}
+  
+}
+
+class AudioController{
+  
+  matchSound: HTMLAudioElement;
+  flipSound: HTMLAudioElement;
+  victorySound: HTMLAudioElement;
+  gameOverSound: HTMLAudioElement;
+ 
+  constructor(){
+    this.flipSound = new Audio('assets/audio/flip.wav');
+    this.matchSound = new Audio('assets/audio/match.wav');
+    this.victorySound = new Audio('assets/audio/victory.wav');
+    this.gameOverSound = new Audio('assets/audio/gameOver.wav');
   }
-  
+
+    flip() {
+        this.flipSound.play();
+    }
+    match() {
+        this.matchSound.play();
+    }
+    victory() {
+        this.victorySound.play();
+    }
+    gameOver() {
+        this.gameOverSound.play();
+    }
 }
